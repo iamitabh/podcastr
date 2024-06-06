8a3222b021295fa18ac33648f09015c2e937c8a6
iamitabh <amitabh.mishra.2002@gmail.com>
2024-06-06 12:15:20 +0530

    add home page trending route

--- app/(root)/create-podcast/page.tsx
+++ app/(root)/create-podcast/page.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const CreatePodcast = () => {
   return (
-    <div>CreatePodcast</div>
+    <div>
+      <h1 className='text-20 font-bold text-white-1'>Trending Podcasts</h1>
+    </div>
   )
 }
 
